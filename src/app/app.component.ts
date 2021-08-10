4cc5b2282811d0091dbb8fb397e0dba7a6070517
Luis Eduardo <luiseperezp.10@gmail.com>
2021-08-09 20:52:08 -0500

    feat(app): create payment gateway form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 
+import { NgCulqiService } from 'ng-culqi';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'culqi-angular';
+  product = {
+    description: 'Artículo #01',
+    amount: 35,
+  };
+  TOKEN_CULQI = '';
+
+  constructor(private ngCulqiService: NgCulqiService) {
+    document.addEventListener('payment_event', (token: any) => {
+      this.TOKEN_CULQI = token.detail;
+    });
+  }
+
+  ngOnInit() {
+    this.ngCulqiService.initCulqi('ENTER_PUBLIC_KEY');
+  }
+
+  payment() {
+    const culqiSettings = {
+      title: 'My Angular Store',
+      currency: 'PEN',
+      description: this.product.description,
+      amount: this.product.amount * 100,
+    };
+    const culqiOptions = {
+      lang: 'auto',
+      modal: true,
+      installments: false,
+      customButton: '',
+      style: {
+        logo: 'https://angularfrontenders.com/wp-content/uploads/2021/01/angular.png',
+        maincolor: '#1976d2',
+        buttontext: '',
+        maintext: '',
+        desctext: '',
+      },
+    };
+    this.ngCulqiService.generateToken(culqiSettings, culqiOptions);
+  }
 }
